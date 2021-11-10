41a0f1198985486ac72f38c78a3b3e9e7802b407
Alexis Viale <alexisviale@ALEXIS-5.local>
2021-11-10 17:20:41 -0300

    segundo commit

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -5,7 +5,7 @@ const SearchResults = ({modoDark, setModoDark, data, trendData, clic, search, is
             <h4 className={`titulo-resultados ${modoDark && `dark`}`}>Resultados de la búsqueda</h4>
             <div className={`cuadro-resultados ${modoDark && `dark-resultados`}`}>
 
-                {(clic && search.length > 0 ? <h1>Loading...</h1> : data.length > 0 ? data.map(items => <img className="gif" onClick={() => window.open(items.images.original.url)} src={items.images.downsized_medium.url} key={items.id}></img>) : (!isTrend) ? null : <h1>No hay resultados</h1>)}
+                {(clic && search.length > 0 ? <img className="gif-loading" src="https://i.gifer.com/origin/b4/b4d657e7ef262b88eb5f7ac021edda87.gif" alt="Loading..." width="50px" height="50px"></img> : data.length > 0 ? data.map(items => <img className="gif" onClick={() => window.open(items.images.original.url)} src={items.images.downsized_medium.url} key={items.id} alt={items.id}></img>) : (!isTrend) ? null : <h1>No hay resultados</h1>)}
 
             </div>
         </div>
